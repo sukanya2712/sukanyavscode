559def4038947a1f72393d85991f903f5e77436e
sukanya2712 <sukanyanaik511@gmail.com>
2022-01-10 17:53:41 +0530

    jan7

---
+++ d24.js
@@ -0,0 +1,27 @@
+<html>
+    <head>
+    <script>
+    let sukanya = {
+        firstName: 'sukanya',
+        lastName: 'naik',
+        array:[2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+        function:{
+            for(x of array)
+            // for(let i =array[0];i<=array.length;i++)
+            {
+                if(array[i]%1==0 && array[i]%array[i]==0){
+                    console.log(array[i])
+                }
+            }
+            
+        }
+         
+    } 
+
+</script>
+    </head>
+    <body>
+
+    </body>
+    </html>
+    
