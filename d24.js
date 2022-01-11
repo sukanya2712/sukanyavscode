b859bf451a2ecccceda993718cd687f89384b8b6
sukanya2712 <sukanyanaik511@gmail.com>
2022-01-11 16:19:32 +0530

    hidde

--- d24.js
+++ d24.js
@@ -1,22 +1,23 @@
 <html>
     <head>
     <script>
-    let sukanya = {
-        firstName: 'sukanya',
-        lastName: 'naik',
-        array:[2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
-        function:{
-            for(x of array)
-            // for(let i =array[0];i<=array.length;i++)
-            {
-                if(array[i]%1==0 && array[i]%array[i]==0){
-                    console.log(array[i])
-                }
-            }
-            
-        }
-         
-    } 
+    function mincost(arr) {
+  arr.sort((a, b) => a-b)// write your code here
+  let cost = 0
+  while (arr.length>1) {
+    let first = arr.shift();
+    let seacond = arr.shift();
+
+    cost += first + seacond
+
+    arr.push(first + seacond);
+    arr.sort((a, b)=>a-b)
+  }
+  return cost
+}
+
+let arr = [4,3,2,6]
+console.log(mincost(arr))
 
 </script>
     </head>
