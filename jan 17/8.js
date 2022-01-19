153a6125aa87bc799ef2a0fc35e515935ec97305
sukanya2712 <sukanyanaik511@gmail.com>
2022-01-19 13:57:47 +0530

    jan 17

--- jan 17/8.js
+++ jan 17/8.js
@@ -2,7 +2,7 @@ const form=document.getElementById("form")
 const input=document.getElementById("input")
 const todosUL=document.getElementById("todos")
 
-// const todo=[
+// const todos=[
 //     {
 //         text:"Abc",
 //         completed:true
@@ -20,17 +20,21 @@ form.addEventListener("submit",(e)=>{
       addTodo()
 })
 
-function addTodo(_todo){
+function addTodo(todo){
     let todoText = input.value;
+
+    // if(todo){
+    //     todoText=todo.text
+    // }
     
     
     if(todoText){
         const todoE1=document.createElement("li")
 
-        // if(todo && todo.completed){
-        //     todoE1.classList.add("completed")
-        // }
-         todoE1.innertext =todoText
+        if(todo && todo.completed){
+            todoE1.classList.add("completed")
+        }
+         todoE1.innerText =todoText
 
         todoE1.addEventListener("click",()=>{
             todoE1.classList.toggle("completed")
