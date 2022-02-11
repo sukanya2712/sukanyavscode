5e6021b366145948138500a8525ab236014f7098
sukanya2712 <95672385+sukanya2712@users.noreply.github.com>
2022-02-11 17:15:44 +0530

    Update script.js

--- project2/script.js
+++ project2/script.js
@@ -1,4 +1,4 @@
-const RANDOM_QUOTE_API_URL = 'https://api.quotable.io/random'
+const RANDOM_QUOTE_API_URL = 'HTTPS://api.quotable.io/random'
 const quoteDisplayElement = document.getElementById('quoteDisplay')
 const quoteInputElement = document.getElementById('quoteInput')
 const timerElement = document.getElementById('timer')
