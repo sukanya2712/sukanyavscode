3c5ddf72fb11b7a20a6ae981fcd0659693e734a0
sukanya2712 <sukanyanaik511@gmail.com>
2022-08-07 19:25:28 +0530

    spotify

---
+++ 7-april/script.js
@@ -0,0 +1,6 @@
+let buttonxyz= document.getElementsByClassName("btn");
+let displayxyz=document.getElementsByClassName("display");
+
+function select(selectno){
+    
+}
---
+++ todolist/todos.js
@@ -0,0 +1,5 @@
+const form=document.getElementById("form");
+const input=document.getElementById("input");
+const ul=document.getElementById("todos");
+const but=document.getElementsByClassName(".add");
+
